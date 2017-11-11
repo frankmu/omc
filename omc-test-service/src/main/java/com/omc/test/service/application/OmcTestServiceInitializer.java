526928af4e13566e98a0f349ead373d9a7c2aa3e
Tengfei Mu <frankluck52@gmail.com>
2017-11-11 15:33:34 -0800

    change service registry data

--- omc-test-service/src/main/java/com/omc/test/service/application/OmcTestServiceInitializer.java
+++ omc-test-service/src/main/java/com/omc/test/service/application/OmcTestServiceInitializer.java
@@ -1,7 +1,11 @@
 package com.omc.test.service.application;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.servlet.ServletContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,19 +16,27 @@ import com.omc.service.registration.OmcServiceRegistry;
 @Component
 public class OmcTestServiceInitializer {
 
+	@Value("${server.port}")
+	private String serverPort;
+
 	@Value("${omc.service.registry.name}")
 	private String omcServiceRegistryName;
 
 	@Autowired
 	OmcServiceRegistry omcServiceRegistry;
 	
+	@Autowired
+    private ServletContext servletContext;
+
 	@PostConstruct
-    public void init() {
-       omcServiceRegistry.registerService(omcServiceRegistryName, "127.0.0.1:8000");
-    }
+	public void init() throws UnknownHostException {
+		String ip = InetAddress.getLocalHost().getHostAddress();
+		omcServiceRegistry.registerService(omcServiceRegistryName, ip + "/" + serverPort + servletContext.getContextPath());
+	}
 
 	@PreDestroy
-	public void destroy() {
-		omcServiceRegistry.unregisterService(omcServiceRegistryName, "127.0.0.1:8000");
+	public void destroy() throws UnknownHostException {
+		String ip = InetAddress.getLocalHost().getHostAddress();
+		omcServiceRegistry.unregisterService(omcServiceRegistryName, ip + "/" + serverPort + servletContext.getContextPath());
 	}
 }
