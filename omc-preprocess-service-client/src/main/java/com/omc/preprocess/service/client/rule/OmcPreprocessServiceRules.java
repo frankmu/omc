ad3fdb3b7580c74e9e15f786e55e829c18518397
Tengfei Mu <frankluck52@gmail.com>
2017-12-09 20:50:49 -0800

    add preprocess client rule

---
+++ omc-preprocess-service-client/src/main/java/com/omc/preprocess/service/client/rule/OmcPreprocessServiceRules.java
@@ -0,0 +1,7 @@
+package com.omc.preprocess.service.client.rule;
+
+import java.util.Map;
+
+public interface OmcPreprocessServiceRules {
+	public Map<String, Object> parse(Map<String, Object> data);
+}
