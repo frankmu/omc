acd07b96842703c8e1a41a5ed74d90520c989687
Tengfei Mu <frankluck52@gmail.com>
2017-12-18 22:20:57 -0800

    change event_ to $ and store_ to @

--- omc-preprocess-service-client/src/main/java/com/omc/preprocess/service/client/rule/OmcPreprocessServiceRules.java
+++ omc-preprocess-service-client/src/main/java/com/omc/preprocess/service/client/rule/OmcPreprocessServiceRules.java
@@ -7,13 +7,13 @@ import java.util.Map;
  * 
  * @param data Original data map from collector service
  * 	e.g. {
- * 			"event_agent": "Syslog_Collector",
- * 			"event_class": "Syslog",
- * 			"event_token0": "Jun  9 09:55:05 192.168.0.131 263030: *Jun  9 10:16:22.732: xxx.xxx.xxx.xx %SNMP-3-AUTHFAIL: Authentication failure for SNMP req from host 192.16.1.18",
- * 			"event_token1": "192.168.0.131",
+ * 			"$agent": "Syslog_Collector",
+ * 			"$class": "Syslog",
+ * 			"$token0": "Jun  9 09:55:05 192.168.0.131 263030: *Jun  9 10:16:22.732: xxx.xxx.xxx.xx %SNMP-3-AUTHFAIL: Authentication failure for SNMP req from host 192.16.1.18",
+ * 			"$token1": "192.168.0.131",
  * 			 ...
- * 			"event_count": 15,
- * 			"event_time": 1509686567
+ * 			"$count": 15,
+ * 			"$time": 1509686567
  * 			
  * 		 }
  * 
@@ -23,11 +23,11 @@ import java.util.Map;
  * 		}
  *
  *  Note: following fields are required fields:
- *  "store_agent"
- *  "store_class"
- *  "store_summary";
- *  "store_occurrence"
- *  "store_identifier"
+ *  "@agent"
+ *  "@class"
+ *  "@summary";
+ *  "@occurrence"
+ *  "@identifier"
  *  "system_dupfield"
  */
 public interface OmcPreprocessServiceRules {
--- omc-service-common/src/main/java/com/omc/service/domain/OmcEventConstant.java
+++ omc-service-common/src/main/java/com/omc/service/domain/OmcEventConstant.java
@@ -2,20 +2,20 @@ package com.omc.service.domain;
 
 public class OmcEventConstant {
 
-	public static final String EVENT_AGENT = "event_agent";
-	public static final String EVENT_CLASS = "event_class";
-	public static final String EVENT_TOKEN_PREFIX = "event_token";
-	public static final String EVENT_COUNT = "event_count";
-	public static final String EVENT_TIME = "event_time";
+	public static final String EVENT_AGENT = "$agent";
+	public static final String EVENT_CLASS = "$class";
+	public static final String EVENT_TOKEN_PREFIX = "$token";
+	public static final String EVENT_COUNT = "$count";
+	public static final String EVENT_TIME = "$time";
 
-	public static final String EVENT_PREFIX = "event_";
-	public static final String STORE_PREFIX = "store_";
+	public static final String EVENT_PREFIX = "$";
+	public static final String STORE_PREFIX = "@";
 	public static final String SYSTEM_PREFIX = "system_";
 
-	public static final String STORE_AGENT = "store_agent";
-	public static final String STORE_CLASS = "store_class";
-	public static final String STORE_SUMMARY = "store_summary";
-	public static final String STORE_OCCURENCE = "store_occurrence";
-	public static final String STORE_IDENTIFIER = "store_identifier";
+	public static final String STORE_AGENT = "@agent";
+	public static final String STORE_CLASS = "@class";
+	public static final String STORE_SUMMARY = "@summary";
+	public static final String STORE_OCCURENCE = "@occurrence";
+	public static final String STORE_IDENTIFIER = "@identifier";
 	public static final String SYSTEM_DEPFIELD = "system_dupfield";
 }
